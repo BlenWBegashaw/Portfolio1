6940514ef909e96734ca417e6c9071990745db96
Blen Begashaw <133809716+BlenWBegashaw@users.noreply.github.com>
2024-10-18 15:22:23 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,25 @@
-import React, { useState, useEffect } from "react";
-import Preloader from "../src/components/Pre";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
-import Resume from "./components/Resume/ResumeNew";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate
-} from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
-import "./style.css";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Projects from "./components/Projects";
+import ChatifyPage from "./components/project-pages/ChatifyPage";
+import AnomalyDetectionPage from "./components/project-pages/AnomalyDetectionPage";
+import SalesforceChatbotPage from "./components/project-pages/SalesforceChatbotPage";
+import SmartHospitalPage from "./components/project-pages/SmartHospitalPage";
 
 function App() {
-  const [load, upadateLoad] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      upadateLoad(false);
-    }, 1200);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <Router>
-      <Preloader load={load} />
-      <div className="App" id={load ? "no-scroll" : "scroll"}>
-        <Navbar />
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/"/>} />
-        </Routes>
-        <Footer />
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Projects />} />
+        <Route path="/chatify" element={<ChatifyPage />} />
+        <Route path="/anomaly-detection" element={<AnomalyDetectionPage />} />
+        <Route path="/salesforce-chatbot" element={<SalesforceChatbotPage />} />
+        <Route path="/smart-hospital" element={<SmartHospitalPage />} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
