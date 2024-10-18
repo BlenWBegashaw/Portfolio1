039eba3026f5252c044a797a807d448f23f69e3c
Blen Begashaw <133809716+BlenWBegashaw@users.noreply.github.com>
2024-10-18 13:32:03 -0400

    Update ProjectCards.js

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -7,34 +7,38 @@ import { BsGithub } from "react-icons/bs";
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Img variant="top" src={props.imgPath} alt="project-image" />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
+        
+        <Button 
+          variant="dark" 
+          href={props.ghLink} 
+          target="_blank"
+          className="project-button"
+        >
           <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
+          {props.isBlog ? "Read Blog" : "GitHub"}
         </Button>
-        {"\n"}
-        {"\n"}
 
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
+        {/* If there's a demo link, it will render the Demo button */}
         {!props.isBlog && props.demoLink && (
           <Button
-            variant="primary"
+            variant="outline-info"
             href={props.demoLink}
             target="_blank"
+            className="project-button"
             style={{ marginLeft: "10px" }}
           >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
+            <CgWebsite /> &nbsp; Demo
           </Button>
         )}
       </Card.Body>
     </Card>
   );
 }
+
 export default ProjectCards;
