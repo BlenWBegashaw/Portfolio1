10da6fde9ef68cd0ba50501b7aa386527b94b3db
Blen Begashaw <133809716+BlenWBegashaw@users.noreply.github.com>
2024-10-18 16:05:13 -0400

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,33 +8,38 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Blen Begashaw </span>
+            from <span className="purple">Addis Ababa, Ethiopia.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently a senior computer science student passionate about software development, AI, and cybersecurity.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have experience as a software engineer at Audacy and an intern at the Catholic Diocese of Arlington, focusing on network security and AI solutions.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            Apart from coding, here are some activities I enjoy:
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Volunteering
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Reading History and Technology Books
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Watching Korean Dramas
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Knitting
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Going on Nature Walks
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "I believe in using technology to create meaningful solutions!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Blen</footer>
         </blockquote>
       </Card.Body>
     </Card>
